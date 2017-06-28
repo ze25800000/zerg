5ca3e26fb7a8c203a78f21569e1ec5772cd40938
ze25800000 <ze258100000@sina.com>
2017-06-28 16:01:43 +0800

    使用闭包写法查询

--- application/api/model/Banner.php
+++ application/api/model/Banner.php
@@ -8,8 +8,13 @@ use think\Db;
 class Banner {
     public static function getBannerByID($id) {
 //        $result = Db::query('select * from banner_item where banner_id=?', [$id]);
+//        $result = Db::table('banner_item')
+//            ->where('banner_id', '=', $id)
+//            ->select();
         $result = Db::table('banner_item')
-            ->where('banner_id', '=', $id)
+            ->where(function ($query) use ($id) {
+                $query->where('banner_id', '=', $id);
+            })
             ->select();
         return $result;
     }
