2a5f1a596fc73a9e580b60209813ca109ab9eb00
ze25800000 <ze258100000@sina.com>
2017-06-28 15:58:56 +0800

    使用查询构造器进行链式调用

--- application/api/model/Banner.php
+++ application/api/model/Banner.php
@@ -7,7 +7,10 @@ use think\Db;
 
 class Banner {
     public static function getBannerByID($id) {
-        $result = Db::query('select * from banner_item where banner_id=?', [$id]);
+//        $result = Db::query('select * from banner_item where banner_id=?', [$id]);
+        $result = Db::table('banner_item')
+            ->where('banner_id', '=', $id)
+            ->select();
         return $result;
     }
 }
