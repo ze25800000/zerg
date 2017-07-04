327d80e52ae1153ef334cf025bb5bbc4123a8163
ze25800000 <ze258100000@sina.com>
2017-07-04 15:13:59 +0800

    下单接口业务模型（2）

--- application/api/service/Order.php
+++ application/api/service/Order.php
@@ -4,6 +4,7 @@ namespace app\api\service;
 
 
 use app\api\model\Product;
+use app\lib\exception\OrderException;
 
 class Order {
     //订单的商品列表，也就是客户端传过来的products参数
@@ -18,6 +19,58 @@ class Order {
         $this->oProducts = $oProducts;
         $this->products  = $this->getProductsByOrder($oProducts);
         $this->uid       = $uid;
+    }
+
+    private function getOrderStatus() {
+        $status = [
+            'pass'         => true,
+            'orderPrice'   => 0,
+            'pStatusArray' => []
+        ];
+        foreach ($this->oProducts as $oProduct) {
+            $pStatus = $this->getProductStatus(
+                $oProduct['product_id'],
+                $oProduct['count'],
+                $this->products
+            );
+            if (!$pStatus['haveStock']) {
+                $status['pass'] = false;
+            }
+            $status['orderPrice'] += $pStatus['totalPrice'];
+            array_push($status['pStatusArray'], $pStatus);
+        }
+        return $status;
+    }
+
+    private function getProductStatus($oPID, $oCount, $products) {
+        $pIndex  = -1;
+        $pStatus = [
+            'id'         => null,
+            'haveStock'  => false,
+            'count'      => 0,
+            'name'       => '',
+            'totalPrice' => 0
+        ];
+        for ($i = 0; $i < count($products); $i++) {
+            if ($oPID == $products[$i]['id']) {
+                $pIndex = $i;
+            }
+        }
+        if ($pIndex == -1) {
+            throw new OrderException([
+                'msg' => 'id为' . $oPID . '的商品不存在，创建订单失败'
+            ]);
+        } else {
+            $product               = $products[$pIndex];
+            $pStatus['id']         = $product['id'];
+            $pStatus['name']       = $product['name'];
+            $pStatus['count']      = $oCount;
+            $pStatus['totalPrice'] = $product['price'] * $oCount;
+            if ($product['stock'] - $oCount >= 0) {
+                $pStatus['haveStock'] = true;
+            }
+        }
+        return $pStatus;
     }
 
     //根据订单信息查找真实的商品信息
---
+++ application/lib/exception/OrderException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace app\lib\exception;
+
+
+class OrderException extends BaseException {
+    public $code = 404;
+    public $msg = '订单不存在';
+    public $errorCode = 80000;
+}
