f7313ae51a1ade1208a474c3329465715e33d6d3
ze25800000 <ze258100000@sina.com>
2017-07-04 17:39:55 +0800

    订单创建

--- application/api/service/Order.php
+++ application/api/service/Order.php
@@ -3,10 +3,12 @@
 namespace app\api\service;
 
 
+use app\api\model\OrderProduct;
 use app\api\model\Product;
 use app\api\model\UserAddress;
 use app\api\validate\UserException;
 use app\lib\exception\OrderException;
+use think\Exception;
 
 class Order {
     //订单的商品列表，也就是客户端传过来的products参数
@@ -30,6 +32,45 @@ class Order {
         $orderSnap = $this->snapOrder($status);
     }
 
+    private function createOrder($snap) {
+        try {
+            $orderNo = $this->makeOrderNo();
+            $order   = new \app\api\model\Order();
+
+            $order->user_id      = $this->uid;
+            $order->order_no     = $orderNo;
+            $order->total_price  = $snap['orderPrice'];
+            $order->total_count  = $snap['totalCount'];
+            $order->snap_img     = $snap['snapImg'];
+            $order->snap_name    = $snap['snapName'];
+            $order->snap_address = $snap['snapAddress'];
+            $order->snap_items   = json_encode($snap['pStatus']);
+
+            $order->save();
+
+            $orderID     = $order->id;
+            $create_time = $order->create_time;
+            foreach ($this->oProducts as &$p) {
+                $p['order_id'] = $orderID;
+            }
+            $orderProduct = new OrderProduct();
+            $orderProduct->saveAll($this->oProducts);
+            return [
+                'order_no'    => $orderNo,
+                'order_id'    => $orderNo,
+                'create_time' => $create_time
+            ];
+        } catch (Exception $ex) {
+            throw $ex;
+        }
+    }
+
+    public static function makeOrderNo() {
+        $yCode   = array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J');
+        $orderSn = $yCode[intval(date('Y')) - 2017] . strtoupper(dechex(date('m'))) . date('d') . substr(time(), -5) . substr(microtime(), 2, 5) . sprintf('%02d', rand(0, 99));
+        return $orderSn;
+    }
+
     //生成订单快照
     private function snapOrder($status) {
         $snap                = [
@@ -49,6 +90,7 @@ class Order {
         if (count($this->products) > 1) {
             $snap['name'] .= '等';
         }
+        return $snap;
     }
 
     private function getUserAddress() {
