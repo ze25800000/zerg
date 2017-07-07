b28416aa521346468a49206dd6afabfbee85b849
ze25800000 <ze258100000@sina.com>
2017-07-07 11:25:45 +0800

    支付回调通知处理

--- application/api/controller/v1/Pay.php
+++ application/api/controller/v1/Pay.php
@@ -18,4 +18,12 @@ class Pay extends BaseController {
         $pay = new PayService($id);
         return $pay->pay();
     }
+
+    public function receiveNotify() {
+        //1.检测库存量
+        //2.更新订单status状态
+        //3.减库存
+        //如果成功处理，我们返回微信成功处理的信息。否则，我们需要返回没有成功处理通知
+        //特点：post：xml格式，不会携带参数
+    }
 }
